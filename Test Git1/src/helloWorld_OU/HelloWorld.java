64566975c41c07f4b82734e40d180c2a814b1954
Hillig-dev <57876538+Hillig-dev@users.noreply.github.com>
2020-11-02 22:03:54 +0100

    seperated the main method and the hello world message into two classes

--- Test Git1/src/helloWorld_OU/HelloWorld.java
+++ Test Git1/src/helloWorld_OU/HelloWorld.java
@@ -2,6 +2,7 @@ package helloWorld_OU;
 
 public class HelloWorld {
 public static void main(String[] args) {
-    System.out.println("hellooo world");
+    HelloWorldClass helloWorldObject = new HelloWorldClass();
+    helloWorldObject.helloWorldMethod();
 }
 }
