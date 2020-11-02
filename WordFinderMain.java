8421aeabf3d6449cc469968e258e5d1bdc4e37f8
Hillig-dev <57876538+Hillig-dev@users.noreply.github.com>
2020-11-02 11:41:20 +0100

    Add files via upload
    .

---
+++ WordFinderMain.java
@@ -0,0 +1,10 @@
+package mattiAllgemein;
+
+public class WordFinderMain {
+
+    public static void main(String[] args) {
+        WordFinder finderobject = new WordFinder();
+        finderobject.wordFinder(args[0],  args[1]);
+    }
+
+}
