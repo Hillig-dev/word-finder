0ba1f42c2edd0211bea85a70ce9dd68b64ef9a39
Hillig-dev <57876538+Hillig-dev@users.noreply.github.com>
2020-11-02 11:47:15 +0100

    Start Ausgabe

--- matti allgemein/src/mattiAllgemein/WordFinderMain.java
+++ matti allgemein/src/mattiAllgemein/WordFinderMain.java
@@ -3,6 +3,7 @@ package mattiAllgemein;
 public class WordFinderMain {
 
     public static void main(String[] args) {
+        System.out.println("*** START ***");
         WordFinder finderobject = new WordFinder();
         finderobject.wordFinder(args[0],  args[1]);
     }
